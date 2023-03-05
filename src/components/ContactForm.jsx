c6a3c43556ca5986db80be83f835fcb32c5d6d03
Santiago Rivarola <srivarola@gmail.com>
2023-03-05 19:43:33 -0300

    contact

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,43 @@
+const ContactForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.form[0].value)
+        console.log(e.target.form[1].value)
+        console.log(e.target.form[2].value)
+    }
+
+  return (
+    <div  className="flex w-full sm:w-[50%] h-[50vh] sm:h-fit sm:px-20">
+        <form className="flex flex-col justify-between p-2 w-full gap-5 sm:gap-10 text-lg border-2 border-primary rounded-lg">
+            <div className="flex flex-col gap-2">
+                <label>Nombre:</label>
+                <input
+                    className="w-full bg-transparent placeholder:italic border-b-2 border-white outline-none px-2"
+                    placeholder="Ingrese su nombre" 
+                    type='text'
+                />
+            </div>
+            <div className="flex flex-col">
+                <label>Email:</label>
+                <input 
+                    className="w-full bg-transparent placeholder:italic border-b-2 border-white outline-none px-2"
+                    placeholder="Ingrese su email"
+                    type='mail'
+                />
+            </div>
+            <div className="flex flex-col">
+                <label>Mensaje:</label>
+                <textarea
+                    className="w-full h-28 bg-transparent placeholder:italic border-b-2 border-white outline-none px-2" 
+                    placeholder="Ingrese un mensaje..."
+                />
+            </div>
+            <div className="flex justify-center">
+                <button type="submit" onClick={(e) => handleSubmit(e)} className="bg-primary/60 py-1 px-6 rounded font-semibold">ENVIAR</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+export default ContactForm
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 import { BsLinkedin, BsGithub, BsWhatsapp } from "react-icons/bs"
+import { SiGmail } from 'react-icons/si'
 import { motion } from "framer-motion"
-import { GoMail } from "react-icons/go"
-import { AiFillCopy } from "react-icons/ai"
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
+import ContactForm from "../components/ContactForm"
 
 const Contact = () => {
   
@@ -53,6 +53,17 @@ const Contact = () => {
                 transition: { duration: 0.5 },
               }}
               whileTap={{ scale: 0.9 }}
+              onClick={() => handleNavigate('https://api.whatsapp.com/send?phone=+541160057411&text=Hola.%20Vi%20tu%20portfolio.')}
+            >
+              <SiGmail />
+            </motion.button>
+            <motion.button
+              className="text-4xl"
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.5 },
+              }}
+              whileTap={{ scale: 0.9 }}
               onClick={() => handleNavigate('https://www.linkedin.com/in/santiago-rivarola/')}
             >
               <BsLinkedin />
@@ -78,77 +89,8 @@ const Contact = () => {
             />
           </div>
         </div>
-        <div  className="flex w-full sm:w-[50%] h-[50vh] sm:h-fit sm:px-20">
-          <form className="flex flex-col justify-between p-2 w-full gap-5 sm:gap-10 text-lg border-2 border-primary rounded-lg">
-            <div className="flex flex-col gap-2">
-              <label>Nombre:</label>
-              <input
-                className="w-full bg-transparent placeholder:italic border-b-2 border-white outline-none px-2"
-                placeholder="Ingrese su nombre" 
-                type='text'
-              />
-            </div>
-            <div className="flex flex-col">
-              <label>Email:</label>
-              <input 
-                className="w-full bg-transparent placeholder:italic border-b-2 border-white outline-none px-2"
-                placeholder="Ingrese su email"
-                type='email'
-              />
-            </div>
-            <div className="flex flex-col">
-              <label>Mensaje:</label>
-              <textarea
-                className="w-full h-28 bg-transparent placeholder:italic border-b-2 border-white outline-none px-2" 
-                placeholder="Ingrese un mensaje..."
-              />
-            </div>
-            <div className="flex justify-center">
-              <button className="bg-primary/60 py-1 px-6 rounded font-semibold">ENVIAR</button>
-            </div>
-          </form>
-        </div>
+        <ContactForm />
       </div>
-      {/* <div className="flex gap-20">
-        <motion.button
-          className="text-4xl"
-          whileHover={{
-            scale: 1.1,
-            transition: { duration: 0.8 },
-          }}
-          whileTap={{ scale: 0.9 }}
-          onClick={() => handleNavigate('https://www.linkedin.com/in/santiago-rivarola/')}
-        >
-          <BsLinkedin />
-        </motion.button>
-        <motion.button
-          className="text-4xl"
-          whileHover={{
-            scale: 1.2,
-            transition: { duration: 0.8 },
-          }}
-          whileTap={{ scale: 0.9 }}
-          onClick={() => handleNavigate('https://github.com/SRivarola')}
-        >
-          <BsGithub />
-        </motion.button>
-      </div>
-      <a className="bg-primary/60 py-2 px-6 rounded font-semibold text-xl hover:bg-primary/70" href="./Santiago Rivarola CV.pdf" download>Descargar CV</a>
-      <div>
-        <img
-          className="w-[300px]" 
-          src="https://firebasestorage.googleapis.com/v0/b/fixturewolrdcup2022.appspot.com/o/portfolio%2Fqrwhatsapp.png?alt=media&token=17d1bf4d-2480-455f-9b0b-4031f89c5940" alt='qr whatsapp' 
-        />
-      </div>
-      <div className="flex gap-8 items-center justify-center text-4xl">
-          <GoMail />
-          <p className="text-xl">srivarola@gmail.com</p>
-          <button onClick={handleCopy} className="text-xl">
-            <CopyToClipboard text='srivarola@gmail.com'>
-              <AiFillCopy />
-            </CopyToClipboard>
-          </button>
-      </div> */}
     </div>
   )
 }

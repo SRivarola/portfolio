629b55cfd83bffffcf26f62c83c011223e313383
Santiago Rivarola <srivarola@gmail.com>
2023-03-05 18:53:46 -0300

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,11 +30,11 @@ const Contact = () => {
   }
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-start items-evenly text-white pt-28">
+    <div className="h-screen w-screen flex flex-col justify-start items-evenly text-white pt-10 sm:pt-28">
       <h2 className="text-center text-3xl text-shadow sm:text-4xl font-semibold">Contacto</h2>
-      <div className="flex p-10 gap-5 w-full">
-        <div className="flex flex-col w-[50%] gap-10">
-          <div className="flex justify-center gap-10">
+      <div className="flex flex-col sm:flex-row p-6 sm:p-8 gap-5 w-full">
+        <div className="flex flex-col w-full sm:w-[50%] p-2 sm:gap-10 ">
+          <div className="flex justify-center flex-wrap sm:flex-nowrap gap-6 sm:gap-10">
             <motion.button
               className="text-4xl"
               whileHover={{
@@ -71,14 +71,33 @@ const Contact = () => {
             </motion.button>
             <a className="bg-primary/60 py-2 px-6 rounded font-semibold text-xl hover:bg-primary/70" href="./Santiago Rivarola CV.pdf" download>Descargar CV</a>
           </div>
-          <div className="w-full flex justify-center">
+          <div className="hidden w-full sm:flex justify-center">
             <img
               className="w-[300px]" 
               src="https://firebasestorage.googleapis.com/v0/b/fixturewolrdcup2022.appspot.com/o/portfolio%2Fqrwhatsapp.png?alt=media&token=17d1bf4d-2480-455f-9b0b-4031f89c5940" alt='qr whatsapp' 
             />
           </div>
         </div>
-        <div  className="bg-white/10 w-[50%]"></div>
+        <div  className="flex w-full h-[50vh] rounded-lg border-2 border-primary">
+          <form className="flex flex-col p-2 w-full gap-5">
+            <div className="flex flex-col gap-2">
+              <label>Nombre:</label>
+              <input
+                className="w-full bg-transparent placeholder:italic border-b-2 border-white outline-none px-2"
+                placeholder="Ingrese su nombre" 
+                type='text'
+              />
+            </div>
+            <div className="flex flex-col">
+              <label>Email:</label>
+              <input />
+            </div>
+            <div className="flex flex-col">
+              <label>Mensaje:</label>
+              <input />
+            </div>
+          </form>
+        </div>
       </div>
       {/* <div className="flex gap-20">
         <motion.button
